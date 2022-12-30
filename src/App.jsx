e0559f56d09f96f9d666f7eaa9f65be6268860fe
cleverAkanimoh <crushclever1@gmail.com>
2022-12-30 02:06:01 +0100

    modified App.jsx in src

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Jokes from './Components/Jokes'
 
 function App() {
   const [jokes, setJokes] = useState(data);
 
   return (
     <div className="App">
-      
+      <Jokes />
     </div>
   )
 }
