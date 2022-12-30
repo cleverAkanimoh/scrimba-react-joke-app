df6209056bae317fd5aa84d3085b00747afadb83
cleverAkanimoh <crushclever1@gmail.com>
2022-12-30 02:41:34 +0100

    modified Jokes.jsx in Components

--- src/Components/Jokes.jsx
+++ src/Components/Jokes.jsx
@@ -2,13 +2,14 @@ const Jokes = ({ jokes }) => {
     return (
         <section id="jokes--container">
             {jokes.map(joke => {
-            const { id, setup, punchline } = joke;
+                const { id, setup, punchline } = joke;
 
                 return <article key={id} id="jokes">
-                        <h3>{setup}</h3>
-                        <p>{punchline}</p>
-                        <hr />
-                    </article>}
+                    <h3>{setup}</h3>
+                    <p>{punchline}</p>
+                    <hr />
+                </article>
+            }
             )}
         </section>
     )
